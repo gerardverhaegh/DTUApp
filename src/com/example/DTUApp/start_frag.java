dc5fe52339d93800e2e83d10064c78af515962d1
gerardverhaegh <verhaegh@pivat.dk>
2015-03-24 23:25:59 +0100

    added date picker

--- src/com/example/DTUApp/start_frag.java
+++ src/com/example/DTUApp/start_frag.java
@@ -3,16 +3,21 @@ package com.example.DTUApp;
 
 //import android.app.Fragment;
 
+import android.app.Activity;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.*;
 
 import java.util.Calendar;
 
@@ -40,6 +45,17 @@ public class start_frag extends Fragment {
             }
         });
 
+        EditText txtDate = (EditText) v.findViewById(R.id.txtDate);
+        txtDate.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+
+                showPopup(getActivity());
+            }
+        });
+
         return v;
     }
 
@@ -51,5 +67,38 @@ public class start_frag extends Fragment {
         time.setTimeInMillis(System.currentTimeMillis());
         time.add(Calendar.SECOND, 10);
         alarmMgr.set(AlarmManager.RTC_WAKEUP, time.getTimeInMillis(), pendingIntent);
+    }
+
+    private void showPopup(Activity context) {
+
+        // Inflate the popup_layout.xml
+        LayoutInflater layoutInflater = (LayoutInflater)getActivity().getBaseContext()
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View layout = layoutInflater.inflate(R.layout.calender, null,false);
+        // Creating the PopupWindow
+        final PopupWindow popupWindow = new PopupWindow(
+                layout,400,400);
+
+        popupWindow.setContentView(layout);
+        popupWindow.setHeight(500);
+        popupWindow.setOutsideTouchable(false);
+        // Clear the default translucent background
+        popupWindow.setBackgroundDrawable(new BitmapDrawable());
+
+        CalendarView cv = (CalendarView) layout.findViewById(R.id.calendarView);
+        cv.setBackgroundColor(Color.BLUE);
+
+        cv.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month,
+                                            int dayOfMonth) {
+                // TODO Auto-generated method stub
+                popupWindow.dismiss();
+                Log.d("date selected", "date selected " + year + " " + month + " " + dayOfMonth);
+
+            }
+        });
+        popupWindow.showAtLocation(layout, Gravity.TOP,5,170);
     }
 }
